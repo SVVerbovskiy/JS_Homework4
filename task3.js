ca83c35f124e9defef73df7f4b95c2ce897ed672
SVVerbovskiy <verbovskii@inbox.ru>
2023-04-11 20:22:01 +0500

    bug fixed

--- task3.js
+++ task3.js
@@ -1,61 +1,53 @@
 const readline = require('readline');
-const fs = require('fs').promises;
+const fs = require('fs');
 
-// Генерируем случайное число от 1 до 100
-const secretNumber = Math.floor(Math.random() * 100) + 1;
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-// Открываем файл для записи протокола игры
-const stream = fs.open('game_log.txt', 'w');
-
-// Счетчик попыток
-let attempts = 0;
-
-// Функция, запрашивающая у пользователя ввод числа
 function question(prompt) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     rl.question(prompt, (answer) => {
-      rl.close();
       resolve(answer);
     });
   });
 }
 
-// Функция, запрашивающая у пользователя ввод числа и возвращающая результат в виде промиса
-async function guessNumber() {
-  // Запрашиваем ввод числа от пользователя
-  const input = await question('Угадайте число от 1 до 100: ');
+function playGame() {
+  const secretNumber = Math.floor(Math.random() * 100) + 1;
+  let guesses = 0;
 
-  // Увеличиваем счетчик попыток
-  attempts++;
+  console.log('Я задумал число от 1 до 100. Вы можете его угадать?');
 
-  // Преобразуем введенный текст в число
-  const guess = parseInt(input);
-
-  // Записываем введенное число в протокол игры
-  await stream.write(`Попытка №${attempts}: ${guess}\n`);
-
-  if (guess === secretNumber) {
-    console.log(`Поздравляем! Вы угадали число за ${attempts} попыток`);
-    // Записываем результат игры в протокол
-    await stream.write(`Результат: Успех (попыток: ${attempts})\n`);
-    // Закрываем файл
-    await stream.close();
-  } else if (guess > secretNumber) {
-    console.log('Загаданное число меньше');
-    await guessNumber();
-  } else {
-    console.log('Загаданное число больше');
-    await guessNumber();
+  function guessLoop() {
+    question('> ')
+      .then((input) => {
+        const guess = parseInt(input);
+        if (isNaN(guess)) {
+          console.log('Пожалуйста введите корректное число');
+          return guessLoop();
+        }
+        guesses++;
+        if (guess === secretNumber) {
+          console.log(`Поздравляем! Вы угадали число за ${guesses} попыток.`);
+          const protocol = `Загаданное число: ${secretNumber}\nПопытки: ${guesses}`;
+          fs.writeFile('protocol.txt', protocol, (err) => {
+            if (err) throw err;
+            console.log('Протокол сохранен в protocol.txt');
+          });
+          rl.close();
+        } else if (guess < secretNumber) {
+          console.log('Загаданное число больше. Попробуйте снова:');
+          return guessLoop();
+        } else {
+          console.log('Загаданное число меньше. Попробуйте снова:');
+          return guessLoop();
+        }
+      });
   }
+
+  guessLoop();
 }
 
-// Начинаем игру
-guessNumber().catch((error) => {
-  console.error(error);
-  process.exit(1);
-});
+playGame();
